c728986266cb39fcf69754de5cb292a80cd1549c
Gonzalo Celan <gonzaloezequielcelan@hotmail.com>
2022-12-20 04:03:49 -0300

    desafio2-manejodearchivos-recuperación

--- ProductManager.js
+++ ProductManager.js
@@ -17,16 +17,19 @@ class ProductManager {
         const dataStr = JSON.stringify(data, null, '\t');
         await fs.writeFile(this.path, dataStr); 
       }
+
     
-      async consultarProducto() {
+      async consultarProduct() {
         if (existsSync(this.path)) {
           return await this.readFile();
-        } else {
-          return [];
+        } else{
+          return []
         }
+
+        
       }
-      async addProduct(pedido) {
-        const product = await this.consultarProducto();
+      async addProduct(order) {
+        const product = await this.consultarProduct();
         if (!product.length)  {
             ProductManager.idCount = 1;
         } else {
@@ -34,13 +37,64 @@ class ProductManager {
         }
         const newProduct = {
           id: ProductManager.idCount,
-          ...pedido
+          ...order
         };
         product.push(newProduct);
         await this.writeFile(product);
         return newProduct;
       }
     
+
+      async getProduct(){
+
+        const readProduct = await this.readFile();
+          return readProduct
+      
+        }
+
+      async getProductById(idProduct){
+       const readProduct = await this.readFile();
+      
+        const searchProductById = readProduct.find(element=>element.id === idProduct)
+        if(!searchProductById){
+          return console.error('PRODUCT NOT FOUND');
+        }
+        else{
+          return searchProductById;
+        }
+
+      }
+
+      async upGradeProduct(idProduct,product){
+        const readProduct = await this.readFile();
+        const productId = (element) => element.id === idProduct;
+
+        const find = readProduct.findIndex(productId)
+        const upGradeproduct = {
+          id: idProduct,
+          ...product
+        }
+
+
+    readProduct.splice(find,1,upGradeproduct);
+     return await this.writeFile(readProduct)
+
+    }
+
+      async deleteProduct(idProduct){
+        const readProduct = await this.readFile();
+      
+        const deleteProductById = readProduct.filter(element=>element.id != idProduct)
+
+       return await this.writeFile(deleteProductById);
+       
+      
+
+      }
+
+
+     
+
    
 
 }
